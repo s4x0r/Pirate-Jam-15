57949882f684f00041e99991eb068afe4c900dd2
s4x0r <s4x0r.101@gmail.com>
2024-07-27 13:24:47 -0500

    fix splash screen

--- final/levels/splash_screen.gd
+++ final/levels/splash_screen.gd
@@ -12,5 +12,5 @@ func _process(delta):
 
 
 func _on_animation_player_animation_finished(anim_name:StringName):
-	get_tree().change_scene_to_file("res://final/start_screen.tscn")
+	get_tree().change_scene_to_file("res://final/levels/start_screen.tscn")
 	pass # Replace with function body.
