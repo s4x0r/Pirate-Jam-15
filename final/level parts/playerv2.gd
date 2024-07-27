b0d4e2747160b04f75978fcd7ab08db85e2d8d22
s4x0r <s4x0r.101@gmail.com>
2024-07-27 13:34:15 -0500

    fix player movement

--- final/level parts/playerv2.gd
+++ final/level parts/playerv2.gd
@@ -53,7 +53,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("player_left", "player_right", "player_forward", "player_back")
 
-	var direction = (Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis*Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
